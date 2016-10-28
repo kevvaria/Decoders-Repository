0d7f7118f4483fedb91730df60e50ec98cd15558
nsidaris <nsidaris@yahoo.com>
2016-10-28 01:06:34 -0700

    Fixes

--- Decoders GUI/debug/moc_mainwindow.cpp
+++ Decoders GUI/debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[47];
-    char stringdata0[838];
+    QByteArrayData data[43];
+    char stringdata0[787];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -48,34 +48,30 @@ QT_MOC_LITERAL(15, 282, 22), // "on_loginButton_clicked"
 QT_MOC_LITERAL(16, 305, 11), // "displayMenu"
 QT_MOC_LITERAL(17, 317, 32), // "on_quantityPurchase_valueChanged"
 QT_MOC_LITERAL(18, 350, 14), // "initializeRest"
-QT_MOC_LITERAL(19, 365, 5), // "dMenu"
-QT_MOC_LITERAL(20, 371, 29), // "on_menuCB_currentIndexChanged"
-QT_MOC_LITERAL(21, 401, 23), // "on_pushButton_2_clicked"
-QT_MOC_LITERAL(22, 425, 12), // "clearReceipt"
-QT_MOC_LITERAL(23, 438, 13), // "updateReceipt"
-QT_MOC_LITERAL(24, 452, 3), // "row"
-QT_MOC_LITERAL(25, 456, 6), // "column"
-QT_MOC_LITERAL(26, 463, 17), // "initializeReceipt"
-QT_MOC_LITERAL(27, 481, 19), // "on_nextRest_clicked"
-QT_MOC_LITERAL(28, 501, 13), // "checkDiabetes"
-QT_MOC_LITERAL(29, 515, 1), // "i"
-QT_MOC_LITERAL(30, 517, 21), // "on_ReturnHome_clicked"
-QT_MOC_LITERAL(31, 539, 11), // "clearReview"
-QT_MOC_LITERAL(32, 551, 30), // "on_actionAdmin_Login_triggered"
-QT_MOC_LITERAL(33, 582, 18), // "on_defMenu_clicked"
-QT_MOC_LITERAL(34, 601, 27), // "on_StartDefaultTrip_clicked"
-QT_MOC_LITERAL(35, 629, 19), // "on_testTrip_clicked"
-QT_MOC_LITERAL(36, 649, 20), // "on_startTrip_clicked"
-QT_MOC_LITERAL(37, 670, 28), // "comboBoxDisplayMenuPrototype"
-QT_MOC_LITERAL(38, 699, 23), // "on_actionExit_triggered"
-QT_MOC_LITERAL(39, 723, 20), // "on_c1SB_valueChanged"
-QT_MOC_LITERAL(40, 744, 30), // "on_restTable_cellDoubleClicked"
-QT_MOC_LITERAL(41, 775, 7), // "getRest"
-QT_MOC_LITERAL(42, 783, 10), // "Restaurant"
-QT_MOC_LITERAL(43, 794, 2), // "re"
-QT_MOC_LITERAL(44, 797, 12), // "getRestIndex"
-QT_MOC_LITERAL(45, 810, 17), // "on_ctPush_clicked"
-QT_MOC_LITERAL(46, 828, 9) // "initcRest"
+QT_MOC_LITERAL(19, 365, 29), // "on_menuCB_currentIndexChanged"
+QT_MOC_LITERAL(20, 395, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(21, 419, 12), // "clearReceipt"
+QT_MOC_LITERAL(22, 432, 13), // "updateReceipt"
+QT_MOC_LITERAL(23, 446, 3), // "row"
+QT_MOC_LITERAL(24, 450, 6), // "column"
+QT_MOC_LITERAL(25, 457, 17), // "initializeReceipt"
+QT_MOC_LITERAL(26, 475, 19), // "on_nextRest_clicked"
+QT_MOC_LITERAL(27, 495, 21), // "on_ReturnHome_clicked"
+QT_MOC_LITERAL(28, 517, 11), // "clearReview"
+QT_MOC_LITERAL(29, 529, 30), // "on_actionAdmin_Login_triggered"
+QT_MOC_LITERAL(30, 560, 18), // "on_defMenu_clicked"
+QT_MOC_LITERAL(31, 579, 27), // "on_StartDefaultTrip_clicked"
+QT_MOC_LITERAL(32, 607, 19), // "on_testTrip_clicked"
+QT_MOC_LITERAL(33, 627, 20), // "on_startTrip_clicked"
+QT_MOC_LITERAL(34, 648, 23), // "on_actionExit_triggered"
+QT_MOC_LITERAL(35, 672, 20), // "on_c1SB_valueChanged"
+QT_MOC_LITERAL(36, 693, 30), // "on_restTable_cellDoubleClicked"
+QT_MOC_LITERAL(37, 724, 7), // "getRest"
+QT_MOC_LITERAL(38, 732, 10), // "Restaurant"
+QT_MOC_LITERAL(39, 743, 2), // "re"
+QT_MOC_LITERAL(40, 746, 12), // "getRestIndex"
+QT_MOC_LITERAL(41, 759, 17), // "on_ctPush_clicked"
+QT_MOC_LITERAL(42, 777, 9) // "initcRest"
 
     },
     "MainWindow\0on_pushButton_6_clicked\0\0"
@@ -88,15 +84,13 @@ QT_MOC_LITERAL(46, 828, 9) // "initcRest"
     "on_pushButton_4_clicked\0on_DistAdd_clicked\0"
     "on_loginButton_clicked\0displayMenu\0"
     "on_quantityPurchase_valueChanged\0"
-    "initializeRest\0dMenu\0on_menuCB_currentIndexChanged\0"
+    "initializeRest\0on_menuCB_currentIndexChanged\0"
     "on_pushButton_2_clicked\0clearReceipt\0"
     "updateReceipt\0row\0column\0initializeReceipt\0"
-    "on_nextRest_clicked\0checkDiabetes\0i\0"
-    "on_ReturnHome_clicked\0clearReview\0"
-    "on_actionAdmin_Login_triggered\0"
+    "on_nextRest_clicked\0on_ReturnHome_clicked\0"
+    "clearReview\0on_actionAdmin_Login_triggered\0"
     "on_defMenu_clicked\0on_StartDefaultTrip_clicked\0"
     "on_testTrip_clicked\0on_startTrip_clicked\0"
-    "comboBoxDisplayMenuPrototype\0"
     "on_actionExit_triggered\0on_c1SB_valueChanged\0"
     "on_restTable_cellDoubleClicked\0getRest\0"
     "Restaurant\0re\0getRestIndex\0on_ctPush_clicked\0"
@@ -110,7 +104,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      39,   14, // methods
+      36,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -118,45 +112,42 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  209,    2, 0x08 /* Private */,
-       3,    0,  210,    2, 0x08 /* Private */,
-       4,    0,  211,    2, 0x08 /* Private */,
-       5,    1,  212,    2, 0x08 /* Private */,
-       7,    0,  215,    2, 0x08 /* Private */,
-       8,    0,  216,    2, 0x08 /* Private */,
-       9,    0,  217,    2, 0x08 /* Private */,
-      10,    0,  218,    2, 0x08 /* Private */,
-      11,    0,  219,    2, 0x08 /* Private */,
-      12,    0,  220,    2, 0x08 /* Private */,
-      13,    0,  221,    2, 0x08 /* Private */,
-      14,    0,  222,    2, 0x08 /* Private */,
-      15,    0,  223,    2, 0x08 /* Private */,
-      16,    0,  224,    2, 0x08 /* Private */,
-      17,    1,  225,    2, 0x08 /* Private */,
-      18,    0,  228,    2, 0x08 /* Private */,
-      19,    0,  229,    2, 0x08 /* Private */,
-      20,    1,  230,    2, 0x08 /* Private */,
-      21,    0,  233,    2, 0x08 /* Private */,
-      22,    0,  234,    2, 0x08 /* Private */,
-      23,    2,  235,    2, 0x08 /* Private */,
-      26,    0,  240,    2, 0x08 /* Private */,
-      27,    0,  241,    2, 0x08 /* Private */,
-      28,    1,  242,    2, 0x08 /* Private */,
-      30,    0,  245,    2, 0x08 /* Private */,
-      31,    0,  246,    2, 0x08 /* Private */,
-      32,    0,  247,    2, 0x08 /* Private */,
-      33,    0,  248,    2, 0x08 /* Private */,
-      34,    0,  249,    2, 0x08 /* Private */,
-      35,    0,  250,    2, 0x08 /* Private */,
-      36,    0,  251,    2, 0x08 /* Private */,
-      37,    0,  252,    2, 0x08 /* Private */,
-      38,    0,  253,    2, 0x08 /* Private */,
-      39,    1,  254,    2, 0x08 /* Private */,
-      40,    2,  257,    2, 0x08 /* Private */,
-      41,    1,  262,    2, 0x08 /* Private */,
-      44,    1,  265,    2, 0x08 /* Private */,
-      45,    0,  268,    2, 0x08 /* Private */,
-      46,    0,  269,    2, 0x08 /* Private */,
+       1,    0,  194,    2, 0x08 /* Private */,
+       3,    0,  195,    2, 0x08 /* Private */,
+       4,    0,  196,    2, 0x08 /* Private */,
+       5,    1,  197,    2, 0x08 /* Private */,
+       7,    0,  200,    2, 0x08 /* Private */,
+       8,    0,  201,    2, 0x08 /* Private */,
+       9,    0,  202,    2, 0x08 /* Private */,
+      10,    0,  203,    2, 0x08 /* Private */,
+      11,    0,  204,    2, 0x08 /* Private */,
+      12,    0,  205,    2, 0x08 /* Private */,
+      13,    0,  206,    2, 0x08 /* Private */,
+      14,    0,  207,    2, 0x08 /* Private */,
+      15,    0,  208,    2, 0x08 /* Private */,
+      16,    0,  209,    2, 0x08 /* Private */,
+      17,    1,  210,    2, 0x08 /* Private */,
+      18,    0,  213,    2, 0x08 /* Private */,
+      19,    1,  214,    2, 0x08 /* Private */,
+      20,    0,  217,    2, 0x08 /* Private */,
+      21,    0,  218,    2, 0x08 /* Private */,
+      22,    2,  219,    2, 0x08 /* Private */,
+      25,    0,  224,    2, 0x08 /* Private */,
+      26,    0,  225,    2, 0x08 /* Private */,
+      27,    0,  226,    2, 0x08 /* Private */,
+      28,    0,  227,    2, 0x08 /* Private */,
+      29,    0,  228,    2, 0x08 /* Private */,
+      30,    0,  229,    2, 0x08 /* Private */,
+      31,    0,  230,    2, 0x08 /* Private */,
+      32,    0,  231,    2, 0x08 /* Private */,
+      33,    0,  232,    2, 0x08 /* Private */,
+      34,    0,  233,    2, 0x08 /* Private */,
+      35,    1,  234,    2, 0x08 /* Private */,
+      36,    2,  237,    2, 0x08 /* Private */,
+      37,    1,  242,    2, 0x08 /* Private */,
+      40,    1,  245,    2, 0x08 /* Private */,
+      41,    0,  248,    2, 0x08 /* Private */,
+      42,    0,  249,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -175,14 +166,11 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int,    6,
     QMetaType::Void,
-    QMetaType::Void,
     QMetaType::Void, QMetaType::QString,    6,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Double, QMetaType::Int, QMetaType::Int,   24,   25,
+    QMetaType::Double, QMetaType::Int, QMetaType::Int,   23,   24,
     QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,   29,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -193,9 +181,9 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int,    6,
-    QMetaType::Void, QMetaType::Int, QMetaType::Int,   24,   25,
-    0x80000000 | 42, QMetaType::QString,   43,
-    QMetaType::Int, 0x80000000 | 42,   43,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,   23,   24,
+    0x80000000 | 38, QMetaType::QString,   39,
+    QMetaType::Int, 0x80000000 | 38,   39,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -224,32 +212,29 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 13: _t->displayMenu(); break;
         case 14: _t->on_quantityPurchase_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 15: _t->initializeRest(); break;
-        case 16: _t->dMenu(); break;
-        case 17: _t->on_menuCB_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 18: _t->on_pushButton_2_clicked(); break;
-        case 19: _t->clearReceipt(); break;
-        case 20: { double _r = _t->updateReceipt((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])));
+        case 16: _t->on_menuCB_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 17: _t->on_pushButton_2_clicked(); break;
+        case 18: _t->clearReceipt(); break;
+        case 19: { double _r = _t->updateReceipt((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])));
             if (_a[0]) *reinterpret_cast< double*>(_a[0]) = _r; }  break;
-        case 21: _t->initializeReceipt(); break;
-        case 22: _t->on_nextRest_clicked(); break;
-        case 23: _t->checkDiabetes((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 24: _t->on_ReturnHome_clicked(); break;
-        case 25: _t->clearReview(); break;
-        case 26: _t->on_actionAdmin_Login_triggered(); break;
-        case 27: _t->on_defMenu_clicked(); break;
-        case 28: _t->on_StartDefaultTrip_clicked(); break;
-        case 29: _t->on_testTrip_clicked(); break;
-        case 30: _t->on_startTrip_clicked(); break;
-        case 31: _t->comboBoxDisplayMenuPrototype(); break;
-        case 32: _t->on_actionExit_triggered(); break;
-        case 33: _t->on_c1SB_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 34: _t->on_restTable_cellDoubleClicked((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
-        case 35: { Restaurant _r = _t->getRest((*reinterpret_cast< QString(*)>(_a[1])));
+        case 20: _t->initializeReceipt(); break;
+        case 21: _t->on_nextRest_clicked(); break;
+        case 22: _t->on_ReturnHome_clicked(); break;
+        case 23: _t->clearReview(); break;
+        case 24: _t->on_actionAdmin_Login_triggered(); break;
+        case 25: _t->on_defMenu_clicked(); break;
+        case 26: _t->on_StartDefaultTrip_clicked(); break;
+        case 27: _t->on_testTrip_clicked(); break;
+        case 28: _t->on_startTrip_clicked(); break;
+        case 29: _t->on_actionExit_triggered(); break;
+        case 30: _t->on_c1SB_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 31: _t->on_restTable_cellDoubleClicked((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 32: { Restaurant _r = _t->getRest((*reinterpret_cast< QString(*)>(_a[1])));
             if (_a[0]) *reinterpret_cast< Restaurant*>(_a[0]) = _r; }  break;
-        case 36: { int _r = _t->getRestIndex((*reinterpret_cast< Restaurant(*)>(_a[1])));
+        case 33: { int _r = _t->getRestIndex((*reinterpret_cast< Restaurant(*)>(_a[1])));
             if (_a[0]) *reinterpret_cast< int*>(_a[0]) = _r; }  break;
-        case 37: _t->on_ctPush_clicked(); break;
-        case 38: _t->initcRest(); break;
+        case 34: _t->on_ctPush_clicked(); break;
+        case 35: _t->initcRest(); break;
         default: ;
         }
     }
@@ -280,13 +265,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 39)
+        if (_id < 36)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 39;
+        _id -= 36;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 39)
+        if (_id < 36)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 39;
+        _id -= 36;
     }
     return _id;
 }
